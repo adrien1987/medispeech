6f20b0b13a2ea2aaa0443d3dc3b03ce3b7594ee1
adrien vidal <andras.adrien@gmail.com>
2018-03-06 18:59:54 +0100

    show du médicament ok

--- app/services/drug_service.rb
+++ app/services/drug_service.rb
@@ -7,4 +7,10 @@ class DrugService
     response = RestClient.get(URL + '?query=' + term)
     JSON.parse(response)
   end
+
+  def self.drug(code_cis)
+    response = RestClient.get(URL + "/" + code_cis)
+    JSON.parse(response)
+  end
+
 end
